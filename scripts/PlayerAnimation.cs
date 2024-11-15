bdb332d9edda50dc6d137b9770076e517d891181
12090113 <47487266+12090113@users.noreply.github.com>
2024-11-15 00:44:39 -0800

    Left arm

--- scripts/PlayerAnimation.cs
+++ scripts/PlayerAnimation.cs
@@ -7,6 +7,7 @@ public partial class PlayerAnimation : AnimatedSprite2D
 	private Player player;
 	private AnimatedSprite2D outline;
 	private Sprite2D rightArm;
+	private Sprite2D leftArm;
 	private float rightArmRotationSpeed = 0.2f;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
@@ -16,21 +17,13 @@ public partial class PlayerAnimation : AnimatedSprite2D
 		grapple = GetParent().GetNode<Grapple>("Grapple");
 		player = GetParent<Player>();
 		rightArm = GetNode<Sprite2D>("RightArm");
+		leftArm = GetNode<Sprite2D>("LeftArm");
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		if (player.LinearVelocity < Vector2.Zero)
-		{
-			this.FlipH = true;
-			outline.FlipH = true;
-		}
-		else
-		{
-			this.FlipH = false;
-			outline.FlipH = false;
-		}
+		Vector2 mousepos = GetGlobalMousePosition();
 		if (grapple.attached)
 		{
 			this.Play("swinging");
@@ -41,7 +34,6 @@ public partial class PlayerAnimation : AnimatedSprite2D
 		{
 			this.Play("idle");
 			outline.Play("idle2");
-			Vector2 mousepos = GetGlobalMousePosition();
 			Vector2 target = (mousepos-player.GlobalPosition).Normalized()*grapple.maxLength;
 			PhysicsDirectSpaceState2D spaceState = GetWorld2D().DirectSpaceState;
 			PhysicsRayQueryParameters2D query = PhysicsRayQueryParameters2D.Create(GlobalPosition, GlobalPosition+target);
@@ -52,6 +44,22 @@ public partial class PlayerAnimation : AnimatedSprite2D
 			} else {
 				rightArm.GlobalRotation = Mathf.LerpAngle(rightArm.GlobalRotation, (player.GlobalPosition+target-rightArm.GlobalPosition).Angle(), grapple.rope.retract <= 0 ? rightArmRotationSpeed : rightArmRotationSpeed/2);
 			}
+		}
+		leftArm.GlobalRotation = Mathf.LerpAngle(leftArm.GlobalRotation, (mousepos-leftArm.GlobalPosition).Angle(), rightArmRotationSpeed);
+		if (Mathf.Cos(rightArm.GlobalRotation) < 0 && Mathf.Cos(leftArm.GlobalRotation) < 0)
+			rightArm.ZIndex = 1;
+		else
+			rightArm.ZIndex = 0;
+		
+		if (Mathf.Cos(leftArm.GlobalRotation) < 0)
+		{
+			this.FlipH = true;
+			outline.FlipH = true;
+		}
+		else
+		{
+			this.FlipH = false;
+			outline.FlipH = false;
 		}
 	}
 }
