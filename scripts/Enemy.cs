f21c447c9415ba1bbb9597a105304fa95b2c9150
TheMangoSpider <matheus.werneck2006@gmail.com>
2024-11-16 17:17:43 -0800

    changes are kind of working
    bullet destroys alien, but tiny aliens dont follow yet
    bullet is supposed to be destroyed when it leaves the screen and when it touches planets/stars but it doesnt yet

--- scripts/Enemy.cs
+++ scripts/Enemy.cs
@@ -1,15 +1,19 @@
 using Godot;
 using System;
+using System.Collections.Generic;
 using System.Threading;
 
-public partial class Enemy : CharacterBody2D
+public partial class Enemy : RigidBody2D
 {
+    [Export]
+	PackedScene tinyEnemyScn;
     [Export] 
     public float Speed = 200f;
     private Node2D _player;
 
     private AnimatedSprite2D outline;
     private AnimatedSprite2D fly;
+    
     public override void _Ready()
     {
         outline = GetNode<AnimatedSprite2D>("Fly/Outline");
@@ -24,11 +28,26 @@ public partial class Enemy : CharacterBody2D
         {
             Vector2 direction = (_player.GlobalPosition - GlobalPosition).Normalized();
 
-            Velocity = direction * Speed;
-            MoveAndSlide();
+            LinearVelocity = direction * Speed;
         }
     }
 
+    public void _on_body_entered(Node body)
+    {
+        if (body.IsInGroup("bullet"))
+        {
+            for (int i = 0; i < 3; i++)
+            {
+            RigidBody2D enemy = tinyEnemyScn.Instantiate<RigidBody2D>();
+            enemy.Rotation = GlobalRotation;
+			enemy.GlobalPosition = GlobalPosition;
+
+            GetTree().Root.AddChild(enemy);
+            }
+            body.QueueFree();
+            QueueFree();
+        }
+    }
     public void playanimation()
     {
         outline.CallDeferred("play");
---
+++ scripts/PlayerBullet.cs
@@ -0,0 +1,40 @@
+using Godot;
+using System;
+
+public partial class PlayerBullet : Node2D
+{	
+	[Export]
+	PackedScene bulletScn;
+	[Export]
+	float bulletSpeed = 2000f;
+	[Export]
+	float bps = 5f;
+	[Export]
+	float bulletDMG = 20f;
+	float fireRate;
+	float timeUntilFire;
+	public override void _Ready()
+	{
+		fireRate = 1 / bps;
+	}
+
+    public override void _Process(double delta)
+    {
+        if (Input.IsActionJustPressed("click") && timeUntilFire > fireRate)
+		{
+			RigidBody2D bullet = bulletScn.Instantiate<RigidBody2D>();
+
+			bullet.Rotation = GlobalRotation;
+			bullet.GlobalPosition = GlobalPosition;
+			bullet.LinearVelocity = bullet.Transform.X * bulletSpeed;
+
+			GetTree().Root.AddChild(bullet);
+
+			timeUntilFire = 0f;
+		}
+		else
+		{
+			timeUntilFire += (float)delta;
+		}
+    }
+}
